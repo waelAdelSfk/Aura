5f5e0f6cad1ff5ae44060e6f4a99d7cea257a54b
waelAdelSfk <waeladelshafik@gmail.com>
2023-10-30 12:38:59 +0200

    update firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyDbHUZOkABGA2QqyfOHouVe_M0XjoLi0WM",
-    authDomain: "offers-b4ec5.firebaseapp.com",
-    projectId: "offers-b4ec5",
-    storageBucket: "offers-b4ec5.appspot.com",
-    messagingSenderId: "416880889907",
-    appId: "1:416880889907:web:b4bcbbb5e79081bf0ad346"
+    apiKey: "AIzaSyCROKjfplMoYBI4QVprjZXjPc0twNTKBUo",
+    authDomain: "offers-notifies.firebaseapp.com",
+    projectId: "offers-notifies",
+    storageBucket: "offers-notifies.appspot.com",
+    messagingSenderId: "1030094650790",
+    appId: "1:1030094650790:web:59847bf6fede31f1416367"
   }
 };
