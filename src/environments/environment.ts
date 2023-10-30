45e4ae93efb01f9371929ef0bc4693370166dd56
waelAdelSfk <waeladelshafik@gmail.com>
2023-10-30 07:26:31 +0200

    change firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,18 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyDwvj2j_1kkBh6YAQfKDCuo27eipFJ_gac",
-    authDomain: "offers-notifier-97114.firebaseapp.com",
-    projectId: "offers-notifier-97114",
-    storageBucket: "offers-notifier-97114.appspot.com",
-    messagingSenderId: "884764178183",
-    appId: "1:884764178183:web:9638672d58517ac0c5f7a2"
+    apiKey: "AIzaSyDbHUZOkABGA2QqyfOHouVe_M0XjoLi0WM",
+    authDomain: "offers-b4ec5.firebaseapp.com",
+    projectId: "offers-b4ec5",
+    storageBucket: "offers-b4ec5.appspot.com",
+    messagingSenderId: "416880889907",
+    appId: "1:416880889907:web:b4bcbbb5e79081bf0ad346"
+    // apiKey: "AIzaSyDwvj2j_1kkBh6YAQfKDCuo27eipFJ_gac",
+    // authDomain: "offers-notifier-97114.firebaseapp.com",
+    // projectId: "offers-notifier-97114",
+    // storageBucket: "offers-notifier-97114.appspot.com",
+    // messagingSenderId: "884764178183",
+    // appId: "1:884764178183:web:9638672d58517ac0c5f7a2"
   }
 };
 
